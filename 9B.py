d9f6fe2d30f470620be2bc1311d969c1c963fbdc
Azelor <kaarel.soot@gmail.com>
2016-04-10 20:51:55 +0300

    Change Eukleidese_algoritm

---
+++ 9B.py
@@ -0,0 +1,24 @@
+1
+parameetrid on ebaselged ja ajavad segadusse.
+Lihtne on eksida nende väljakirjutamisel.
+
+2
+tehete ümber tühikute kasutamine ebaühtlane
+
+3
+Treppimisvead
+
+4
+Mõttetu kommentaar lõpus
+
+
+
+
+
+
+
+
+
+
+1 ddef
+2 trepp
--- Eukleidese_algoritm.py
+++ Eukleidese_algoritm.py
@@ -1,7 +1,7 @@
 def gcd(a, b):
     "Arvude a ja b suurim ühistegur"
     #Tsükkel kestab kuni b on nullist suurem
-    while b > 0:
+    while b > 0:232
         r = a % b
         a = b
         b = r
