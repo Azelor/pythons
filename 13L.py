1f7575c73a7d170c2cdfeed1b031ec6eee1264c0
Azelor <kaarel.soot@gmail.com>
2016-04-26 11:28:18 +0300

    Update loeng13

--- 13L.py
+++ 13L.py
@@ -22,3 +22,34 @@ def valiksortimine(loend):
 print(valiksortimine([3, 4, 1, 9, 4, 4]))
     
 
+def kiirsorditud(loend):
+    """
+    Tagastab loendi elementide järjestatud variandi
+
+    Ajaline keerukus on n log n
+    """
+    if len(loend) <= 1:
+        return loend
+
+    juhtelement = loend[0]
+
+    vaiksemad = []
+    vordsed = []
+    suuremad = []
+
+    for element in loend:
+        if element < juhtelement:
+            vaiksemad.append(element)
+        elif element == juhtelement:
+            vordsed.append(element)
+        elif element > juhtelement:
+            suuremad.append(element)
+    # Sordi rekursiivselt juhtelemendist väiksemad
+    vaiksemad = kiirsorditud(vaiksemad)
+    # Sordi rekursiivselt juhtelemendist suuremad
+    suuremad = kiirsorditud(suuremad)
+    # Tagasta väiksemad + võrdsed + suuremad
+
+    return vaiksemad + vordsed + suuremad
+
+print(kiirsorditud([3, 4, 1, 9, 4, 4]))
